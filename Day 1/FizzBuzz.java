302574b1b946b7568976f96f89995e964cb1ec5d
dsdsharma <dev2sharma2k@gmail.com>
2018-12-21 20:01:26 +0530

    Day 1 Completed with Readme file

---
+++ Day 1/FizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FizzBuzz{
+	
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		System.out.println("====FizzBuzz====");
+		System.out.print("Enter the number for Fizz Buzz iteration: ");
+		int num = input.nextInt();
+		
+		for(int i = 1; i<=num;i++){
+			
+			if(i%15==0){
+				System.out.print("FizzBuzz ");
+			}else if(i%3==0){
+				System.out.print("Fizz ");
+			}else if(i%5==0){
+				System.out.print("Buzz ");
+			}else{
+				System.out.println(i + " ");
+			}
+			
+		}
+	}
+}
